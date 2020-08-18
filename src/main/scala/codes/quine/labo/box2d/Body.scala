7c9ca300b0cdad6d8e89b915faf6936718368f54
TSUYUSATO Kitsune <make.just.on@gmail.com>
2020-08-18 09:35:27 +0900

    feat: add Body

---
+++ src/main/scala/codes/quine/labo/box2d/Body.scala
@@ -0,0 +1,46 @@
+package codes.quine.labo.box2d
+
+final class Body {
+  val position: Vec2 = Vec2(0.0f, 0.0f)
+  var rotation: Float = 0.0f
+
+  val velocity: Vec2 = Vec2(0.0f, 0.0f)
+  var angularVelocity: Float = 0.0f
+
+  val force: Vec2 = Vec2(0.0f, 0.0f)
+  var torque: Float = 0.0f
+  var friction: Float = 0.2f
+
+  var width: Vec2 = Vec2(1.0f, 1.0f)
+  var mass: Float = Float.MaxValue
+  var invMass: Float = 0.0f
+  var I: Float = Float.MaxValue
+  var invI: Float = 0.0f
+
+  def addForce(f: Vec2): Unit = {
+    force += f
+  }
+
+  def set(w: Vec2, m: Float): Unit = {
+    position.set(0.0f, 0.0f)
+    rotation = 0.0f
+    velocity.set(0.0f, 0.0f)
+    angularVelocity = 0.0f
+    force.set(0.0f, 0.0f)
+    torque = 0.0f
+    friction = 0.2f
+
+    width = w
+    mass = m
+
+    if (mass < Float.MaxValue) {
+      invMass = 1.0f / mass
+      I = mass * (width.x * width.x + width.y * width.y) / 12.0f
+      invI = 1.0f / I
+    } else {
+      invMass = 0.0f
+      I = Float.MaxValue
+      invI = 0.0f
+    }
+  }
+}
---
+++ src/test/scala/codes/quine/labo/box2d/BodySuite.scala
@@ -0,0 +1,59 @@
+package codes.quine.labo.box2d
+
+import minitest.SimpleTestSuite
+
+object BodySuite extends SimpleTestSuite {
+  def assertInitialBody(body: Body): Unit = {
+    assertEquals(body.position, Vec2(0, 0))
+    assertEquals(body.rotation, 0)
+    assertEquals(body.velocity, Vec2(0, 0))
+    assertEquals(body.angularVelocity, 0)
+    assertEquals(body.force, Vec2(0, 0))
+    assertEquals(body.torque, 0)
+    assertEquals(body.friction, 0.2f)
+  }
+
+  test("new Body") {
+    val body = new Body
+    assertInitialBody(body)
+    assertEquals(body.width, Vec2(1, 1))
+    assertEquals(body.mass, Float.MaxValue)
+    assertEquals(body.invMass, 0)
+    assertEquals(body.I, Float.MaxValue)
+    assertEquals(body.invI, 0)
+  }
+
+  test("Body#addForce") {
+    val body = new Body
+    body.addForce(Vec2(1, 2))
+    assertEquals(body.force, Vec2(1, 2))
+    body.addForce(Vec2(3, 4))
+    assertEquals(body.force, Vec2(4, 6))
+  }
+
+  test("Body#set") {
+    val body = new Body
+    body.position.set(0, 1)
+    body.rotation = 3
+    body.velocity.set(4, 5)
+    body.angularVelocity = 6
+    body.force.set(7, 8)
+    body.torque = 9
+    body.friction = 10
+
+    body.set(Vec2(1, 1), Float.MaxValue)
+    assertInitialBody(body)
+    assertEquals(body.width, Vec2(1, 1))
+    assertEquals(body.mass, Float.MaxValue)
+    assertEquals(body.invMass, 0)
+    assertEquals(body.I, Float.MaxValue)
+    assertEquals(body.invI, 0)
+
+    body.set(Vec2(1, 2), 3)
+    assertEquals(body.width, Vec2(1, 2))
+    assertEquals(body.mass, 3)
+    assertEquals(body.invMass, 1 / 3.0f)
+    assertEquals(body.I, 3 * 5 / 12.0f)
+    assertEquals(body.invI, 12.0f / (3 * 5))
+  }
+}
